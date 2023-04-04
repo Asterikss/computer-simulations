fa16b3e447f0a4a65aba30cd6cafa86f77ca0864
Andre <AndreAndreM@proton.me>
2023-04-05 00:22:08 +0200

    init

---
+++ rolling_objects/rolling_objects.py
@@ -0,0 +1,14 @@
+import math
+import logging
+# level = logging.DEBUG
+level = logging.INFO
+fmt = "%(levelname)s:%(lineno)d:%(funcName)s: %(message)s"
+logging.basicConfig(level = level, format = fmt)
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
