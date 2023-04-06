ebbc2124e4552d16616c424c36d2f9150a640c7d
Andre <AndreAndreM@proton.me>
2023-04-06 22:25:37 +0200

    add midpoint meth

--- rolling_objects/rolling_objects.py
+++ rolling_objects/rolling_objects.py
@@ -30,7 +30,7 @@ class Vars:
     d90 = math.radians(90)  
     logging.debug(d90)
 
-def func1():
+def simul_euler():
     print("func1")
 
     t = 0
@@ -54,12 +54,7 @@ def func1():
     x = Vars.r * math.cos(Vars.d90 - b) + x_r
     y = Vars.r * math.sin(Vars.d90 - b) + y_r
 
-    sh = 0
-    sh2 = Vars.r * math.cos(sh)
-    sh3 = Vars.r * math.sin(sh)
-
-
-    print("  t     :    s_x     :     s_y   :    v     :     ds_x     :     dv   :   x_r    :    y_r     :     b     :     w     :      db     :      dw    :    x   :   y")
+    print("  t     :    s_x     :     s_y   :    v     :    ds_x   :   dv   :   x_r    :    y_r     :     b     :     w     :    db     :    dw    :    x   :   y")
     print(f"{t:.6f} : {s_x:.6f} : {s_y:.6f}  : {v:.6f} : {ds_x:.6f} : {dv:.6f} : {x_r:.6f} : {y_r:.6f}: {b:.6f}  : {w:.6f}  : {db:.6f}  : {dw:.6f} : {x:.6f} : {y:.6f}")
 
     for _ in range(20):
@@ -81,16 +76,80 @@ def func1():
         x = Vars.r * math.cos(Vars.d90 - b) + x_r
         y = Vars.r * math.sin(Vars.d90 - b) + y_r
 
-        sh += math.pi / 10
-        sh2 = Vars.r * math.cos(sh)
-        sh3 = Vars.r * math.sin(sh)
-
         print(f"{t:.6f} : {s_x:.6f} : {s_y:.6f}  : {v:.6f} : {ds_x:.6f} : {dv:.6f} : {x_r:.6f} : {y_r:.6f}: {b:.6f}  : {w:.6f}  : {db:.6f}  : {dw:.6f} : {x:.6f} : {y:.6f}")
 
 
+def simul_midpoint():
+    print("simul midpoint")
+
+    t = 0
+    s_x = 0
+    s_y = Vars.r
+    v = 0
+
+    v_mid = v + (Vars.acc * Vars.dt)/2
+    a_mid = Vars.acc + (Vars.eps * Vars.dt)/2
+    ds_x = v_mid * Vars.dt
+    dv = a_mid * Vars.dt
+
+    x_r_mid = (s_x + (v_mid * Vars.dt)) * math.cos(-(Vars.alfa)) - (s_y + (v_mid * Vars.dt)) * math.sin(-(Vars.alfa))
+    y_r_mid = (s_x + (v_mid * Vars.dt)) * math.sin(-(Vars.alfa)) + (s_y + (v_mid * Vars.dt)) * math.cos(-(Vars.alfa)) + Vars.h
+
+    # x_r = s_x  * math.cos(-(Vars.alfa)) - s_y * math.sin(-(Vars.alfa))
+    # logging.debug(x_r)
+    # y_r = s_x  * math.sin(-(Vars.alfa)) + s_y * math.cos(-(Vars.alfa)) + Vars.h
+    # logging.debug(y_r)
+
+    b = 0
+    w = 0 #
+    # db = w * Vars.dt
+    # dw = Vars.eps * Vars.dt
+    db = (w + (Vars.eps * Vars.dt)/2) * Vars.dt
+    dw = Vars.eps * Vars.dt
+
+    # x = Vars.r * math.cos(Vars.d90 - b) + x_r
+    # y = Vars.r * math.sin(Vars.d90 - b) + y_r
+    x = Vars.r * math.cos(Vars.d90 - b) + x_r_mid
+    y = Vars.r * math.sin(Vars.d90 - b) + y_r_mid
+
+    print("  t     :    s_x     :     s_y   :    v     :    ds_x   :   dv   :   x_r    :    y_r     :     b     :     w     :    db     :    dw    :    x   :   y")
+    print(f"{t:.6f} : {s_x:.6f} : {s_y:.6f}  : {v:.6f} : {ds_x:.6f} : {dv:.6f} : {x_r_mid:.6f} : {y_r_mid:.6f}: {b:.6f}  : {w:.6f}  : {db:.6f}  : {dw:.6f} : {x:.6f} : {y:.6f}")
+
+    for _ in range(20):
+        t += Vars.dt
+        s_x += ds_x
+        # v += dv * Vars.dt
+        v += dv
+
+        # ds_x = v * Vars.dt
+        v_mid = v + (Vars.acc * Vars.dt)/2
+        a_mid = Vars.acc + (Vars.eps * Vars.dt)/2
+        ds_x = v_mid * Vars.dt
+        dv = a_mid * Vars.dt
+
+        x_r_mid = (s_x + (v_mid * Vars.dt)) * math.cos(-(Vars.alfa)) - (s_y + (v_mid * Vars.dt)) * math.sin(-(Vars.alfa))
+        y_r_mid = (s_x + (v_mid * Vars.dt)) * math.sin(-(Vars.alfa)) + (s_y + (v_mid * Vars.dt)) * math.cos(-(Vars.alfa)) + Vars.h
+        # x_r = s_x  * math.cos(-(Vars.alfa)) - s_y * math.sin(-(Vars.alfa))
+        # y_r = s_x  * math.sin(-(Vars.alfa)) + s_y * math.cos(-(Vars.alfa)) + Vars.h
+
+        b += db
+        w += dw
+        # db = w * Vars.dt
+        # dw = Vars.eps * Vars.dt
+        db = (w + (Vars.eps * Vars.dt)/2) * Vars.dt
+        dw = Vars.eps * Vars.dt
+
+        # x = Vars.r * math.cos(Vars.d90 - b) + x_r
+        # y = Vars.r * math.sin(Vars.d90 - b) + y_r
+        x = Vars.r * math.cos(Vars.d90 - b) + x_r_mid
+        y = Vars.r * math.sin(Vars.d90 - b) + y_r_mid
+
+        print(f"{t:.6f} : {s_x:.6f} : {s_y:.6f}  : {v:.6f} : {ds_x:.6f} : {dv:.6f} : {x_r_mid:.6f} : {y_r_mid:.6f}: {b:.6f}  : {w:.6f}  : {db:.6f}  : {dw:.6f} : {x:.6f} : {y:.6f}")
+
 
 def main():
-    func1()
+    # simul_euler()
+    simul_midpoint()
 
 
 if __name__ == "__main__":
