fe88d861e38683969eb1a9f4629960703e6bd412
Andre <AndreAndreM@proton.me>
2023-04-06 23:13:47 +0200

    impl midpoint

--- rolling_objects/rolling_objects.py
+++ rolling_objects/rolling_objects.py
@@ -1,7 +1,7 @@
 import math
 import logging
-level = logging.DEBUG
-# level = logging.INFO
+# level = logging.DEBUG
+level = logging.INFO
 fmt = "%(levelname)s:%(lineno)d:%(funcName)s: %(message)s"
 logging.basicConfig(level = level, format = fmt)
 
@@ -30,8 +30,8 @@ class Vars:
     d90 = math.radians(90)  
     logging.debug(d90)
 
-def simul_euler():
-    print("func1")
+def sim_euler():
+    print("sim euler")
 
     t = 0
     s_x = 0
@@ -47,7 +47,7 @@ def simul_euler():
     logging.debug(y_r)
 
     b = 0
-    w = 0 #
+    w = 0
     db = w * Vars.dt
     dw = Vars.eps * Vars.dt
 
@@ -60,7 +60,6 @@ def simul_euler():
     for _ in range(20):
         t += Vars.dt
         s_x += ds_x
-        # v += dv * Vars.dt
         v += dv
 
         ds_x = v * Vars.dt
@@ -79,8 +78,8 @@ def simul_euler():
         print(f"{t:.6f} : {s_x:.6f} : {s_y:.6f}  : {v:.6f} : {ds_x:.6f} : {dv:.6f} : {x_r:.6f} : {y_r:.6f}: {b:.6f}  : {w:.6f}  : {db:.6f}  : {dw:.6f} : {x:.6f} : {y:.6f}")
 
 
-def simul_midpoint():
-    print("simul midpoint")
+def sim_midpoint():
+    print("sim midpoint")
 
     t = 0
     s_x = 0
@@ -95,33 +94,23 @@ def simul_midpoint():
     x_r_mid = (s_x + (v_mid * Vars.dt)) * math.cos(-(Vars.alfa)) - (s_y + (v_mid * Vars.dt)) * math.sin(-(Vars.alfa))
     y_r_mid = (s_x + (v_mid * Vars.dt)) * math.sin(-(Vars.alfa)) + (s_y + (v_mid * Vars.dt)) * math.cos(-(Vars.alfa)) + Vars.h
 
-    # x_r = s_x  * math.cos(-(Vars.alfa)) - s_y * math.sin(-(Vars.alfa))
-    # logging.debug(x_r)
-    # y_r = s_x  * math.sin(-(Vars.alfa)) + s_y * math.cos(-(Vars.alfa)) + Vars.h
-    # logging.debug(y_r)
-
     b = 0
-    w = 0 #
-    # db = w * Vars.dt
-    # dw = Vars.eps * Vars.dt
+    w = 0
+
     db = (w + (Vars.eps * Vars.dt)/2) * Vars.dt
     dw = Vars.eps * Vars.dt
 
-    # x = Vars.r * math.cos(Vars.d90 - b) + x_r
-    # y = Vars.r * math.sin(Vars.d90 - b) + y_r
     x = Vars.r * math.cos(Vars.d90 - b) + x_r_mid
     y = Vars.r * math.sin(Vars.d90 - b) + y_r_mid
 
-    print("  t     :    s_x     :     s_y   :    v     :    ds_x   :   dv   :   x_r    :    y_r     :     b     :     w     :    db     :    dw    :    x   :   y")
-    print(f"{t:.6f} : {s_x:.6f} : {s_y:.6f}  : {v:.6f} : {ds_x:.6f} : {dv:.6f} : {x_r_mid:.6f} : {y_r_mid:.6f}: {b:.6f}  : {w:.6f}  : {db:.6f}  : {dw:.6f} : {x:.6f} : {y:.6f}")
+    print("  t     :    s_x     :     s_y   :    v     :    ds_x   :   dv   :   x_r    :    y_r     :     b    :    w    :   db    :    dw    :    x   :   y")
+    print(f"{t:.6f} : {s_x:.6f} : {s_y:.6f} : {v:.6f} : {ds_x:.6f} : {dv:.6f} : {x_r_mid:.6f} : {y_r_mid:.6f}: {b:.6f} : {w:.6f} : {db:.6f} : {dw:.6f}:{x:.6f}:{y:.6f}")
 
-    for _ in range(20):
+    for _ in range(120):
         t += Vars.dt
         s_x += ds_x
-        # v += dv * Vars.dt
         v += dv
 
-        # ds_x = v * Vars.dt
         v_mid = v + (Vars.acc * Vars.dt)/2
         a_mid = Vars.acc + (Vars.eps * Vars.dt)/2
         ds_x = v_mid * Vars.dt
@@ -129,27 +118,22 @@ def simul_midpoint():
 
         x_r_mid = (s_x + (v_mid * Vars.dt)) * math.cos(-(Vars.alfa)) - (s_y + (v_mid * Vars.dt)) * math.sin(-(Vars.alfa))
         y_r_mid = (s_x + (v_mid * Vars.dt)) * math.sin(-(Vars.alfa)) + (s_y + (v_mid * Vars.dt)) * math.cos(-(Vars.alfa)) + Vars.h
-        # x_r = s_x  * math.cos(-(Vars.alfa)) - s_y * math.sin(-(Vars.alfa))
-        # y_r = s_x  * math.sin(-(Vars.alfa)) + s_y * math.cos(-(Vars.alfa)) + Vars.h
 
         b += db
         w += dw
-        # db = w * Vars.dt
-        # dw = Vars.eps * Vars.dt
+
         db = (w + (Vars.eps * Vars.dt)/2) * Vars.dt
         dw = Vars.eps * Vars.dt
 
-        # x = Vars.r * math.cos(Vars.d90 - b) + x_r
-        # y = Vars.r * math.sin(Vars.d90 - b) + y_r
         x = Vars.r * math.cos(Vars.d90 - b) + x_r_mid
         y = Vars.r * math.sin(Vars.d90 - b) + y_r_mid
 
-        print(f"{t:.6f} : {s_x:.6f} : {s_y:.6f}  : {v:.6f} : {ds_x:.6f} : {dv:.6f} : {x_r_mid:.6f} : {y_r_mid:.6f}: {b:.6f}  : {w:.6f}  : {db:.6f}  : {dw:.6f} : {x:.6f} : {y:.6f}")
+        print(f"{t:.6f} : {s_x:.6f} : {s_y:.6f} : {v:.6f} : {ds_x:.6f} : {dv:.6f} : {x_r_mid:.6f} : {y_r_mid:.6f}: {b:.6f} : {w:.6f} : {db:.6f} : {dw:.6f}:{x:.6f}:{y:.6f}")
 
 
 def main():
-    # simul_euler()
-    simul_midpoint()
+    sim_euler()
+    sim_midpoint()
 
 
 if __name__ == "__main__":
