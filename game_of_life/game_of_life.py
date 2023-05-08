f47898554ca625ac62695a961f8d6b256bd26021
Andre <AndreAndreM@proton.me>
2023-05-08 18:39:18 +0200

    imvroved game loop + change rules

--- game_of_life/game_of_life.py
+++ game_of_life/game_of_life.py
@@ -18,24 +18,24 @@ def update(screen: pygame.Surface, cells: NDArray, size: int, rules, with_evolut
         n_alive_around = np.sum(cells[row - 1: row +2, col -1: col +2]) - cells[row, col]
         color = Color.Background if cells[row, col] == 0 else Color.Alive
 
-        if with_evolution:
+        # if with_evolution:
 
-            if cells[row, col] == 1:
-                if rules[1][0] <= n_alive_around <= rules[1][1]:
-                    update_cells[row, col] = 1
-                    if with_evolution:
-                        color = Color.Alive #
+        if cells[row, col] == 1:
+            if rules[1][0] <= n_alive_around <= rules[1][1]:
+                update_cells[row, col] = 1
+                if with_evolution:
+                    color = Color.Alive #
 
-                elif n_alive_around < rules[1][0] or n_alive_around > rules[1][1]:
-                    if with_evolution:
-                        color = Color.Background #
+            elif n_alive_around < rules[1][0] or n_alive_around > rules[1][1]:
+                if with_evolution:
+                    color = Color.Background #
 
 
-            else:
-                if n_alive_around == rules[0]:
-                    update_cells[row, col] = 1
-                    if with_evolution:
-                        color = Color.Alive
+        else:
+            if n_alive_around == rules[0]:
+                update_cells[row, col] = 1
+                if with_evolution:
+                    color = Color.Alive
 
 
         pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
@@ -43,20 +43,51 @@ def update(screen: pygame.Surface, cells: NDArray, size: int, rules, with_evolut
     return update_cells
 
 
-def choosen_rules(rules):
-    print("Available default rules:")
-    for i, rule in enumerate(rules):
-        print(f"nr{i + 1} - {rule[0]} neighbours to be born. {rule[1][0]} <= neighbours to stay alivea <= {rule[1][1]}")
+def choose_rules(rules, first_time=False):
+    if not first_time:
+        # print("Evolution stopped")
+        print("If you want to add custom rules input 'r'. To just change them hit enter")
 
-    print("Input the index number of the rules you want to choose.")
-    print("You will be able to change them later and add custom rules.")
-    print("To do that, while the simulation is running, press space to stop it.")
-    print("You will be prompted for an input then.")
-    while True:
-        chooice = int(input(": "))
-        if chooice in range(0, len(rules)):
-            return chooice
-        print("Enter valid input")
+        if str(input(": ")).lower() == "r":
+            n_be_born = int(input("Enter n of neighbours to be born: "))
+            n_to_live1 =  int(input("Enter minimal number of neighbours to survive: "))
+            n_to_live2 = int(input("Enter maximal number of neighbours to survive: "))
+
+            rules.append((n_be_born, (n_to_live1, n_to_live2)))
+
+
+        print("Available rules:")
+        for i, rule in enumerate(rules):
+            print(f"nr{i + 1} - {rule[0]} neighbours to be born. {rule[1][0]} <= neighbours to stay alivea <= {rule[1][1]}")
+
+        while True:
+            chooice = int(input("Input the index of the choosen rules: "))
+            if chooice in range(0, len(rules) + 1):
+                print("Input cells / click space to resume if the simulation is stopped")
+                return chooice - 1, rules
+            print("Enter valid input")
+
+        # else:
+        #     print("Input cells / click space to resume")
+        #     return previous_chooice, rules
+
+
+    else:
+        print("Available rules:")
+        for i, rule in enumerate(rules):
+            print(f"nr{i + 1} - {rule[0]} neighbours to be born. {rule[1][0]} <= neighbours to stay alivea <= {rule[1][1]}")
+
+        print("Input the index number of the rules you want to choose.")
+        print("You will be able to change them later and add custom rules.")
+        print("To do that, while the simulation is running, press r.")
+        print("You will be prompted for an input then.")
+
+        while True:
+            chooice = int(input("Input the index of the choosen rules: "))
+            if chooice in range(0, len(rules) + 1):
+                return chooice - 1, rules
+            print("Enter valid input")
+
 
 
 def main():
@@ -64,7 +95,7 @@ def main():
     # (n to be born, (x <= n to stay alive <= x)
     rules: list[tuple[int, tuple[int, int]]] = [(3, (2,3)), (2, (2, 7)), (2, (3, 4))]
 
-    choosen_rules_idx = choosen_rules(rules)
+    choosen_rules_idx, rules = choose_rules(rules, first_time=True)
 
     size = 10
 
@@ -93,8 +124,17 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     paused = not paused
-                    update(screen, cells, 10, rules[choosen_rules_idx], False) # should be here
+
+                    if paused:
+                        print("~~~Game paused. Hit enter to unpause")
+                    else:
+                        print("+++Game running. Hit enter to pause")
+
+                    update(screen, cells, 10, rules[choosen_rules_idx], False) #
                     pygame.display.update() #
+
+                if event.key == pygame.K_r:
+                        choosen_rules_idx, rules = choose_rules(rules)
 
             if pygame.mouse.get_pressed()[0]:
                 # maby prevent from inserting when its running
