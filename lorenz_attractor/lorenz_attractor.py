b5d039fba1205a66ded55ec80c4c97b57af89d00
Andre <AndreAndreM@proton.me>
2023-05-11 19:32:54 +0200

    init

---
+++ lorenz_attractor/lorenz_attractor.py
@@ -0,0 +1,25 @@
+import logging
+
+
+class DefaultVars():
+    x = 1
+    y = 1
+    z = 1
+
+    a = 10
+    b = 25
+    c = 8/3
+
+    dt = 0.03
+
+
+def lorenz_euler():
+    pass
+
+
+def main():
+    lorenz_euler()
+
+
+if __name__ == "__main__":
+    main()
