adbf14e3044382795b9504805c83b52b31ba1837
Andre <AndreAndreM@proton.me>
2023-05-11 23:57:59 +0200

    that was tough

--- lorenz_attractor/lorenz_attractor.py
+++ lorenz_attractor/lorenz_attractor.py
@@ -1,4 +1,3 @@
-
 class Vars():
     x = 1
     y = 1
@@ -24,28 +23,31 @@ def lorenz_euler():
 
     dt = Vars.dt
 
-    d_x = (a * y - a * x) * dt
-    d_y = (-1 * x * z + b * x - y) * dt
-    d_z = (x * y - c * z) * dt
+    # d_x = (a * y - a * x) * dt
+    # d_y = (-1 * x * z + b * x - y) * dt
+    # d_z = (x * y - c * z) * dt
     print(f"  t   :   x   :   y   :   z   :   d_x   :   d_y  :  d_z")
-    print(f"{t:.3f} : {x:.3f} : {y:.3f} : {z:.3f} : {d_x:.3f} : {d_y:.3f} : {d_z:.3f}")
+    # print(f"{t:.3f} : {x:.3f} : {y:.3f} : {z:.3f} : {d_x:.3f} : {d_y:.3f} : {d_z:.3f}")
 
     for _ in range(20):
         t += dt
-
-        x += d_x
-        y += d_y
-        z += d_z
 
         d_x = (a * y - a * x) * dt
         d_y = (-1 * x * z + b * x - y) * dt
         d_z = (x * y - c * z) * dt
 
+        x += d_x
+        y += d_y
+        z += d_z
+
+        # d_x = (a * y - a * x) * dt
+        # d_y = (-1 * x * z + b * x - y) * dt
+        # d_z = (x * y - c * z) * dt
+
         print(f"{t:.3f} : {x:.3f} : {y:.3f} : {z:.3f} : {d_x:.3f} : {d_y:.3f} : {d_z:.3f}")
 
 
-
-def lorenz_imprv_euler():
+def lorenz_improved_euler():
     print("improved euler")
     t = 0
     x = Vars.x
@@ -58,111 +60,24 @@ def lorenz_imprv_euler():
 
     dt = Vars.dt
 
-    
-
-    d_x = (a * y - a * x) * dt
-    d_y = (-1 * x * z + b * x - y) * dt
-    d_z = (x * y - c * z) * dt
-    print(f"  t   :   x   :   y   :   z   :   d_x   :   d_y  :  d_z")
-    print(f"{t:.2f} : {x:.3f} : {y:.3f} : {z:.3f} : {d_x:.3f} : {d_y:.3f} : {d_z:.3f}")
-
-    for _ in range(40):
-        t += dt
-
-        x += d_x
-        y += d_y
-        z += d_z
-        
-        # mid_x = x + ((a * y - a * x) * dt/2)
-        # mid_y = y + ((-1 * x * z + b * x - y) * dt/2)
-        # mid_z = z + ((x * y - c * z) * dt/2)
-        mid_x = x + ((a * y - a * x) * dt/2)
-        mid_y = y + ((-1 * x * z + b * x - y) * dt/2)
-        mid_z = z + ((x * y - c * z) * dt/2)
-
-        # d_x = (a * y - a * x) * dt
-        # d_y = (-1 * x * z + b * x - y) * dt
-        # d_z = (x * y - c * z) * dt
-        d_x = mid_x * dt
-        d_y = mid_y * dt
-        d_z = mid_z * dt
-
-        print(f"{t:.3f} : {x:.3f} : {y:.3f} : {z:.3f} : {d_x:.3f} : {d_y:.3f} : {d_z:.3f}")
-
-
-def lorenz_improved_euler2():
-    print("improved euler2")
-    t = 0
-    x = Vars.x
-    y = Vars.y
-    z = Vars.z
-
-    a = Vars.a
-    b = Vars.b
-    c = Vars.c
-
-    dt = Vars.dt
-
     print(f"  t   :   x   :   y   :   z   :   d_x   :   d_y  :  d_z")
     print(f"{t:.3f} : {x:.3f} : {y:.3f} : {z:.3f} : 0.000 : 0.000 : 0.000")
 
-    # for _ in range(120):
+    # for _ in range(220):
     for _ in range(20):
         t += dt
 
-        # Calculate intermediate values using Euler method
-        x_i = x + (a * y - a * x) * dt
-        y_i = y + (-1 * x * z + b * x - y) * dt
-        z_i = z + (x * y - c * z) * dt
+        x_i = x + ((a * y - a * x) * dt/2)
+        y_i = y + ((-1 * x * z + b * x - y) * dt/2)
+        z_i = z + ((x * y - c * z) * dt/2)
 
-        # Calculate new values using intermediate values and Euler method
-        d_x = (a * y_i - a * x_i) * dt
-        d_y = (-1 * x_i * z_i + b * x_i - y_i) * dt
-        d_z = (x_i * y_i - c * z_i) * dt
+        d_x = (a * y_i - a * x_i)
+        d_y = (-1 * x_i * z_i + b * x_i - y_i)
+        d_z = (x_i * y_i - c * z_i)
 
-        x += 0.5 * (d_x + (a * y - a * x) * dt)
-        y += 0.5 * (d_y + (-1 * x * z + b * x - y) * dt)
-        z += 0.5 * (d_z + (x * y - c * z) * dt)
-
-        print(f"{t:.3f} : {x:.3f} : {y:.3f} : {z:.3f} : {d_x:.3f} : {d_y:.3f} : {d_z:.3f}")
-
-
-def lorenz_improved_euler3():
-    print("improved euler3")
-    t = 0
-    x = Vars.x
-    y = Vars.y
-    z = Vars.z
-
-    a = Vars.a
-    b = Vars.b
-    c = Vars.c
-
-    dt = Vars.dt
-
-    print(f"  t   :   x   :   y   :   z   :   d_x   :   d_y  :  d_z")
-    print(f"{t:.3f} : {x:.3f} : {y:.3f} : {z:.3f} : 0.000 : 0.000 : 0.000")
-
-    # for _ in range(120):
-    for _ in range(220):
-        t += dt
-
-        # Calculate intermediate values using Euler method
-        x_i = x + (a * y - a * x) * (dt/2)
-        y_i = y + (-1 * x * z + b * x - y) * (dt/2)
-        z_i = z + (x * y - c * z) * (dt/2)
-
-        # Calculate new values using intermediate values and Euler method
-        d_x = (a * y_i - a * x_i) * dt
-        d_y = (-1 * x_i * z_i + b * x_i - y_i) * dt
-        d_z = (x_i * y_i - c * z_i) * dt
-
-        # x += 0.5 * (d_x + (a * y - a * x) * dt)
-        # y += 0.5 * (d_y + (-1 * x * z + b * x - y) * dt)
-        # z += 0.5 * (d_z + (x * y - c * z) * dt)
-        x += (d_x + (a * y - a * x) * dt)
-        y += (d_y + (-1 * x * z + b * x - y) * dt)
-        z += (d_z + (x * y - c * z) * dt)
+        x += d_x * dt
+        y += d_y * dt
+        z += d_z * dt
 
         print(f"{t:.3f} : {x:.3f} : {y:.3f} : {z:.3f} : {d_x:.3f} : {d_y:.3f} : {d_z:.3f}")
 
@@ -211,10 +126,8 @@ def lorenz_rk4():
 
 
 def main():
-    # lorenz_euler()
-    # lorenz_imprv_euler()
-    # lorenz_improved_euler2()
-    # lorenz_improved_euler3()
+    lorenz_euler()
+    lorenz_improved_euler()
     lorenz_rk4()
 
 
