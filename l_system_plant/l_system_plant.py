2f18b9f2762da74fd7cd2078dfe3c95685ae396a
Andre <AndreAndreM@proton.me>
2023-05-22 23:40:38 +0200

    init l system

---
+++ l_system_plant/l_system_plant.py
@@ -0,0 +1,53 @@
+import turtle
+from collections import deque
+
+
+def l_system_plant():
+    turtle.bgcolor('black')
+    t = turtle.Turtle()
+    t.color('green')
+    t.left(90)
+
+    stack = deque()
+
+    w = "X"
+    for _ in range(4):
+        tmp_w = ""
+        for char in w:
+            if char == "F":
+                tmp_w += "FF"
+            elif char == "X":
+                tmp_w += "F+[[X]-X]-F[-FX]+X"
+            else:
+                tmp_w += char
+
+        w = tmp_w
+
+    for char in w:
+        if char == "F":
+            t.forward(15)
+        elif char == "+":
+            t.right(25)
+        elif char == "-":
+            t.left(25)
+        elif char == "[":
+            # stack.append(t.pos())
+            stack.append((t.pos(), t.heading()))
+        elif char == "]":
+            t.penup()
+            pos, heading = stack.pop()
+            t.setpos(pos)
+            t.setheading(heading)
+            t.pendown()
+
+    turtle.done()
+    while True:
+        ...
+
+
+def main():
+    l_system_plant()
+
+
+if __name__ == "__main__":
+    main()
